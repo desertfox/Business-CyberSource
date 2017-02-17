e364a05295f783c9a7c89d83182f906f6dee4f50
John Rummel <cerealbh@gmail.com>
2017-02-17 11:30:47 -0600

    Added InvoiceHeader RequestPart. Added InvoiceHeader to Capture. Added InvoiceHeader helper service to breadboard. Added test for InvoiceHeader. Modified capture test.

--- lib/Business/CyberSource/Request/Capture.pm
+++ lib/Business/CyberSource/Request/Capture.pm
@@ -8,16 +8,24 @@ use namespace::autoclean;
 use Moose;
 extends 'Business::CyberSource::Request';
 with qw(
-	Business::CyberSource::Request::Role::DCC
-	Business::CyberSource::Request::Role::TaxService
+  Business::CyberSource::Request::Role::DCC
+  Business::CyberSource::Request::Role::TaxService
 );
 
-use MooseX::Types::CyberSource qw( CaptureService );
+use MooseX::Types::CyberSource qw( CaptureService  InvoiceHeader );
 
 has '+service' => (
-	isa         => CaptureService,
-	remote_name => 'ccCaptureService',
-	lazy_build  => 0,
+    isa         => CaptureService,
+    remote_name => 'ccCaptureService',
+    lazy_build  => 0,
+);
+
+has 'invoice_header' => (
+    isa         => InvoiceHeader,
+    remote_name => 'invoiceHeader',
+    is          => 'ro',
+    required    => 0,
+    coerce      => 1
 );
 
 __PACKAGE__->meta->make_immutable;
@@ -38,6 +46,11 @@ __PACKAGE__->meta->make_immutable;
 			total    => 5.01,  # same amount as in authorization
 			currency => 'USD', # same currency as in authorization
 		},
+        invoice_header => {
+            purchaser_vat_registration_number => 'ATU99999999',
+            user_po => '123456',
+            vat_invoice_reference_number => '1234',
+        },
 	});
 
 =head1 DESCRIPTION
---
+++ lib/Business/CyberSource/RequestPart/InvoiceHeader.pm
@@ -0,0 +1,60 @@
+package Business::CyberSource::RequestPart::InvoiceHeader;
+use strict;
+use warnings;
+use namespace::autoclean;
+
+# VERSION
+
+use Moose;
+extends 'Business::CyberSource::MessagePart';
+with 'MooseX::RemoteHelper::CompositeSerialization';
+
+use MooseX::Types::CyberSource qw(
+  _VarcharThirteen
+  _VarcharFifteen
+  _VarcharTwentyFive
+);
+
+has 'purchaser_vat_registration_number' => (
+    isa         => _VarcharThirteen,
+    is          => 'rw',
+    remote_name => 'purchaserVATRegistrationNumber',
+);
+
+has 'user_po' => (
+    isa         => _VarcharTwentyFive,
+    is          => 'rw',
+    remote_name => 'userPO',
+);
+
+has 'vat_invoice_reference_number' => (
+    isa         => _VarcharFifteen,
+    is          => 'rw',
+    remote_name => 'vatInvoiceReferenceNumber',
+);
+
+__PACKAGE__->meta->make_immutable;
+1;
+
+# ABSTRACT: InvoiceHeader information
+
+=head1 EXTENDS
+
+L<Business::CyberSource::MessagePart>
+
+=attr purchaser_vat_registration_number
+
+Identification number assigned to the purchasing company by the tax
+authorities .
+
+=attr user_po 
+
+Value used by your customer to identify the order. This value is typically a purchase order number. 
+
+=attr vat_invoice_reference_number 
+
+VAT invoice number associated with the transaction.
+
+=for Pod::Coverage BUILD
+
+=cut;
